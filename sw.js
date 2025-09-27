ff9b7ebcdd9a21f2715fb5a23d82fea11842a0a3
trashcore-dev <trashcoreclient@gmail.com>
2025-09-27 23:09:11 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,6 @@
+self.addEventListener('install', (e) => {
+    console.log('Service Worker installed');
+});
+self.addEventListener('fetch', (e) => {
+    e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
